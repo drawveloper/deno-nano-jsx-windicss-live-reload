337b0e0da420e110806124617f790ecc88f96e3d
Guilherme Rodrigues <gadr90@gmail.com>
2022-02-13 23:13:49 -0300

    Remove console

--- src/server-timing.ts
+++ src/server-timing.ts
@@ -7,13 +7,10 @@ export function createServerTimingMiddleware() {
     end: (key: string) => timingValues[key] = Date.now() - timingValues[key],
     serverTimingMiddleware: async (context: Context, next: () => Promise<unknown>) => {
       await next();
-      console.log(timingValues);
       const result = Object.keys(timingValues).reduce((acc: string, key: string) => {
         const timing = timingValues[key]
         return acc + `${key};dur=${timing}, `
       }, '')
-
-      console.log('timing', result)
       context.response.headers.set("Server-Timing", result);
     }
   }
